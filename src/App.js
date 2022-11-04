b2a24bfb21ff21a61bc70e2f96760c99d13f7850
praveen kumar <praveen.k@madstreetden.com>
2022-11-04 19:54:09 +0530

    Added: navbar js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo}  alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://direct.me/schumyvannakaviyangal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Schummy Vanna Kaviyangal
         </a>
       </header>
     </div>
