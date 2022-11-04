146526ef9f0b07f89374917954c33394435be679
praveen kumar <praveen.k@madstreetden.com>
2022-11-04 19:53:41 +0530

    Added: Navbar component

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Navbar.css';
+
+
+
+const Navbar = () => {
+    return (
+        <>
+        <div className="Navbar">
+            <div>Navbar</div>
+            <div>Navbar</div>
+            <div>Logo</div>
+            <div>Navbar</div>
+            <div>Navbar</div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
