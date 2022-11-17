661dd6aa9e642e3cf679be0db9ff1bc30f90e471
sudhan <sudhan6496@gmail.com>
2022-11-18 00:36:37 +0530

    created Stats page

---
+++ src/Stats.js
@@ -0,0 +1,33 @@
+import React from 'react';
+const Stat = () => {
+    return (
+        <>
+            <div>
+                <h1 align = "right">Stats page</h1>
+            </div>
+           
+            <div>
+                <h2 align = "left">Time line graph</h2>
+            </div>
+
+            <div>
+                <h2 align = "left">Total Views</h2>                
+                <div class="card text-white bg-primary mb-3">
+                    <div class="card-header">Total Views</div>
+                    <div class="card-body">
+                        <h5 class="card-title">99,999,999</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h2 align = "right">Active listners</h2>
+            </div>
+
+
+
+        </>
+    );
+}
+
+export default Stat
