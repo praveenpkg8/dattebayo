58956d55ec15ab146f44b29a2c0e57f448d447a5
SathiyamV <sathiyamsharmila@gmail.com>
2022-12-21 03:57:17 +0530

    80 per completed

--- src/Donation.js
+++ src/Donation.js
@@ -7,12 +7,14 @@ const Donation = () => {
       <body>
         <div class="donationMainContainer">
           <div class="donationHeader">
-            <br></br>
             <h1>DONATIONS</h1>
           </div>
           <div class="donationcontainer">
             <div class="donbox">
               <h2>BUY ME A COFFEE</h2>
+              <div class=" perk">
+                <h3>Perks</h3>
+              </div>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               </p>
@@ -24,6 +26,9 @@ const Donation = () => {
             </div>
             <div class="patbox">
               <h2>PATREON</h2>
+              <div class=" perk">
+                <h3>Perks</h3>
+              </div>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               </p>
@@ -34,6 +39,9 @@ const Donation = () => {
             </div>
             <div class="ytbox">
               <h2>13 FAMILY PACK</h2>
+              <div class=" perk">
+                <h3>Perks</h3>
+              </div>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               </p>
