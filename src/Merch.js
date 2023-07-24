5828c05fa5f7b53c54b1b33b429314a33888ed15
SathiyamV <sathiyamsharmila@gmail.com>
2023-07-24 21:29:53 +0530

    responsive

--- src/Merch.js
+++ src/Merch.js
@@ -2,17 +2,6 @@ import React from "react";
 import "./css/Merch.css";
 
 import URLS from "./urls.json";
-import svkLargeLogo from "./svkLargeLogo.png";
-import haashiraamaaSocial from "./assets/socialMedia/haashi.png";
-import logoSocial from "./assets/socialMedia/logoSocial.png";
-import tobiSocial from "./assets/socialMedia/tobi.png";
-import KakashiSocial from "./assets/socialMedia/Kakashi.png";
-import kisameSocial from "./assets/socialMedia/kisame.png";
-import smSocial from "./assets/socialMedia/sm.png";
-
-import kisamelogo from "./assets/kisame_avatar.png";
-import tobilogo from "./assets/tobi_avatar.png";
-import kakashilogo from "./assets/kakashi_avatar.png";
 
 const Merch = (_, ref) => {
   return (
